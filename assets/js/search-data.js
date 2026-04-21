19b6d96300f066ef12067b3d4cbf4f3ed6536ea4
guiomarpescador <40167900+guiomarpescador@users.noreply.github.com>
2026-04-21 17:11:36 +0000

    Deploying to gh-pages from @ guiomarpescador/guiomarpescador.github.io@3f1ebde7a18a06278d4a044fbcc91e46607ecc8f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
+  },{id: "nav-research",
+          title: "Research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
+        },{id: "nav-publications",
           title: "publications",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
-          },
-        },{id: "nav-",
-          title: "",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/research/";
           },
         },{id: "nav-cv",
           title: "cv",
